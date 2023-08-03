a7e568d51605834767f4e94dcac406ffa6697178
boostup <boostup@gmail.com>
2023-08-03 11:48:41 +0200

    feat: create form was rebuilt using useFieldArray

--- src/modules/Workspace/WorkspaceForm/Toolbar.tsx
+++ src/modules/Workspace/WorkspaceForm/Toolbar.tsx
@@ -1,57 +1,22 @@
 import { Link } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
-export default ({onSelectDate}) => (
+export default ({ onSelectDate }) => (
     <>
-        <h2 className="font-semibold text-slate-900">
+        <h2 className="page-actions mt-0 font-semibold text-slate-900 justify-between">
             <Link to={"/workspace"}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
-                </svg></Link>
-        </h2>
-        <section className="page-actions mt-0">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                </svg>
 
-            <Popover>
-                <PopoverTrigger asChild>
-                    <Button className="btn-primary">
-                        + Date
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent className="top-4">
-                    <Calendar
-                        initialFocus
-                        mode="single"
-                        // defaultMonth={date?.from}
-                        // selected={date}
-                        onSelect={onSelectDate}
-                    // numberOfMonths={2}
-                    />
-                </PopoverContent>
-            </Popover>
-
-            <Popover>
-                <PopoverTrigger asChild>
-                    <Button className="btn-primary">
-                        + Plage de dates
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    <Calendar
-                        initialFocus
-                        mode="range"
-                    // defaultMonth={date?.from}
-                    // selected={date}
-                    // onSelect={addDate}
-                    // numberOfMonths={2}
-                    />
-                </PopoverContent>
-            </Popover>
-            <Button className="btn-primary">
+            </Link>
+            <Button className="btn-primary gap-5">
                 Envoyer
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                </svg>
             </Button>
-        </section>
+        </h2>
     </>
 )
