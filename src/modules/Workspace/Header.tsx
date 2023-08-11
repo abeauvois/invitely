9c2fb3d9b8e8fc646792dbf03bcd3da0fe900ba3
boostup <boostup@gmail.com>
2023-08-11 22:24:21 +0200

    feat: forms are now searchable via input text

--- src/modules/Workspace/Header.tsx
+++ src/modules/Workspace/Header.tsx
@@ -1,23 +1,68 @@
+import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { PageActions } from "@/shared/components/PageActions";
+import { CrossCircledIcon, MagnifyingGlassIcon, PlusIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/utils";
 
-export const Header = ({ onNewForm }) => (
-  <>
-    <PageActions backTo={{ url: "/", label: "Home" }} pageTitle={"Workspace"}>
-      <Button onClick={onNewForm} variant="primary">
-        <svg width="20" height="20" fill="currentColor" className="mr-2" aria-hidden="true">
-          <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-        </svg>
-        New
-      </Button>
-    </PageActions>
-    <div className="flex justify-center">
-      <form className="group relative">
-        <svg width="20" height="20" fill="currentColor" className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
-          <path fillRule="evenodd" clipRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
-        </svg>
-        <input className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter forms" placeholder="Filter forms..." />
-      </form>
-    </div>
-  </>
-);
+const ClearFormButton = ({ displayCondition, setValue, onClear }) => {
+
+  const clear = (e) => {
+    setValue("searchTerm", "");
+    onClear(e);
+  };
+
+  return (
+    <Button
+      type="reset"
+      onClick={clear}
+      variant="secondary"
+      className={cn(displayCondition ? "visible" : "invisible", "relative -left-12 top-0.5")}
+    >
+      <CrossCircledIcon />
+    </Button >
+  )
+}
+
+export const Header = ({ onNewForm, onSearch }) => {
+
+  const { handleSubmit, setValue, register, watch } = useForm({
+    defaultValues: {
+      searchTerm: ""
+    }
+  });
+  const { name, ref, onBlur, onChange } = register("searchTerm");
+  const handleChange = (e) => {
+    onChange(e);
+    onSearch(e?.target.value || "");
+  };
+  const searchTerm = watch("searchTerm");
+  const onSubmit = data => false;
+
+  return (
+    <>
+      <PageActions backTo={{ url: "/", label: "Home" }} pageTitle={"Workspace"}>
+        <Button onClick={onNewForm} variant="primary">
+          <PlusIcon className="w-3 h-3 mr-2" />
+          Formulaire
+        </Button>
+      </PageActions>
+      <div className="flex justify-center">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="group relative">
+          <div className="flex items-center">
+            <MagnifyingGlassIcon width="20" height="20" fill="currentColor" className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" />
+            <input
+              {...{ name, ref, onBlur }}
+              onChange={handleChange}
+              className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 pr-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter forms" placeholder="Chercher un formulaire..." />
+            <ClearFormButton
+              displayCondition={searchTerm.length ? true : false}
+              setValue={setValue}
+              onClear={handleChange} />
+          </div>
+        </form>
+      </div >
+    </>
+  )
+};
--- src/modules/Workspace/Workspace.tsx
+++ src/modules/Workspace/Workspace.tsx
@@ -17,16 +17,25 @@ export const Workspace = () => {
         return getFormIds();
     });
 
+    const handleSearch = (searchTerm) => {
+        setFormIds(
+            getFormIds().filter(
+                formId => forms[formId].title?.toLowerCase()
+                    .includes(searchTerm?.toLowerCase())
+            )
+        );
+    };
+
     return (
         <section className="page">
-            <Header onNewForm={handleNewForm} />
+            <Header onNewForm={handleNewForm} onSearch={handleSearch} />
             <ul className="p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4 text-sm leading-6">
                 {formIds.map((formId, i) => (
-                    <FormCard 
-                        key={formId} 
-                        to={`/workspace/form/${formId}/`} 
-                        label={forms[formId].title} 
-                        onDelete={() => handleDeleteForm({formId})} />
+                    <FormCard
+                        key={formId}
+                        to={`/workspace/form/${formId}/`}
+                        label={forms[formId].title}
+                        onDelete={() => handleDeleteForm({ formId })} />
                 ))}
                 <AddCard onNewForm={handleNewForm} />
             </ul>
