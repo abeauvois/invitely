91d0047477638ffe66f6fd778ee3c7c81bce856c
boostup <boostup@gmail.com>
2023-08-29 22:05:56 +0200

    chore: finished routes migration (from BrowserRouter to RouterProvider).

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider, Routes, Route, useNavigate, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, useNavigate, createBrowserRouter, Outlet } from "react-router-dom";
 
 import {
   ClerkProvider,
@@ -31,109 +31,96 @@ if (!env.VITE_CLERK_PUBLISHABLE_KEY) {
 
 const clerkPubKey = env.VITE_CLERK_PUBLISHABLE_KEY;
 
-function ClerkProviderWithRoutes() {
-  const navigate = useNavigate();
-
-  return (
-    <ClerkProvider
-      publishableKey={clerkPubKey}
-      navigate={(to) => navigate(to)}
-    >
-      <ClerkLoaded>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<Landing />} />
-          </Route>
-          <Route
-            path="/sign-in/*"
-            element={<SignIn routing="path" path="/sign-in" />}
-          />
-          <Route
-            path="/sign-up/*"
-            element={<SignUp routing="path" path="/sign-up" />}
-          />
-          <Route element={<PageLayout />}>
-            <Route path="workspace/send" element={
-              <>
-                <SendForm username="nickname" email="a@b.c" />
-              </>
-            } />
-          </Route>
-        </Routes>
-      </ClerkLoaded>
-    </ClerkProvider>
-  );
-}
-
-// ***************************
-// MIGRATION from BrowserRouter to RouterProvider.
-// 
-// https://reactrouter.com/en/main/upgrading/v6-data
-
-// 1️⃣ Changed from App to Root
-export const Root = (): React.ReactElement | null => {
-  // 2️⃣ `BrowserRouter` component removed, but the <Routes>/<Route>
-  // component below are unchanged
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ClerkProviderWithRoutes />
-    </QueryClientProvider>
-  );
-};
-
-// 3️⃣ Router singleton created
 const router = createBrowserRouter([
   {
-    Component: PageLayout,
+    Component: () => {
+      const navigate = useNavigate();
+      return (
+        <QueryClientProvider client={queryClient}>
+          <ClerkProvider
+            publishableKey={clerkPubKey}
+            navigate={(to) => navigate(to)}
+          >
+            <ClerkLoaded>
+              <Outlet />
+            </ClerkLoaded>
+          </ClerkProvider>
+        </QueryClientProvider>
+      )
+    },
     children: [
       {
-        path: "workspace",
-        loader: workspaceLoader,
-        Component: () => (
-          <>
-            <SignedIn>
-              <Workspace />
-            </SignedIn>
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
-          </>
-        )
+        Component: AppLayout,
+        children: [
+          {
+            path: "/",
+            Component: Landing
+          },
+        ]
       },
       {
-        path: "workspace/form/:formId/",
-        loader: workspaceFormLoader,
-        Component: () => (
-          <>
-            <SignedIn>
-              <WorkspaceForm />
-            </SignedIn>
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
-          </>
-        )
+        path: "/sign-in/*",
+        Component: () => <SignIn routing="path" path="/sign-in" />
       },
       {
-        path: "workspace/form/:formId/:recipientId/",
-        loader: workspaceFormReplyLoader,
-        Component: () => (
-          <>
-            <SignedIn>
-              <WorkspaceFormReply />
-            </SignedIn>
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
-          </>
-        )
-      }
+        path: "/sign-up/*",
+        Component: () => <SignUp routing="path" path="/sign-up" />
+      },
+      {
+        Component: PageLayout,
+        children: [
+          {
+            path: "workspace",
+            loader: workspaceLoader,
+            Component: () => (
+              <>
+                <SignedIn>
+                  <Workspace />
+                </SignedIn>
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              </>
+            )
+          },
+          {
+            path: "workspace/form/:formId/",
+            loader: workspaceFormLoader,
+            Component: () => (
+              <>
+                <SignedIn>
+                  <WorkspaceForm />
+                </SignedIn>
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              </>
+            )
+          },
+          {
+            path: "workspace/form/:formId/:recipientId/",
+            loader: workspaceFormReplyLoader,
+            Component: () => (
+              <>
+                <SignedIn>
+                  <WorkspaceFormReply />
+                </SignedIn>
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              </>
+            )
+          },
+          {
+            path: "workspace/send",
+            Component: () => <SendForm username="nickname" email="a@b.c" />
+          }
+        ]
+      },
     ]
   },
-  { path: "*", Component: Root },
 ]);
 
-// 4️⃣ RouterProvider added
 export function App() {
   return <RouterProvider router={router} />;
 }
