9709966838e1409df27ad312499d3bac05e3e940
boostup <boostup@gmail.com>
2023-08-30 10:51:51 +0200

    feat: database errors are now allowed to bubble up to the app-wide error handling

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,22 +2,13 @@ import { getDatabase, ref, set, get } from "firebase/database";
 import { app } from "@/config/firebase";
 
 export async function getDbData({ location = "/" }) {
-    try {
-        const db = getDatabase(app);
-        const query = ref(db, location);
-        const snapshot = await get(query);
-        return snapshot.val();
-    } catch (error) {
-        console.log("error from getLC:", error);
-        return {};
-    }
+    const db = getDatabase(app);
+    const query = ref(db, location);
+    const snapshot = await get(query);
+    return snapshot.val();
 }
 
 export function setDbData({ location, toStore }) {
-    try {
-        const db = getDatabase();
-        return set(ref(db, location), toStore);
-    } catch (error) {
-        return {};
-    }
+    const db = getDatabase();
+    return set(ref(db, location), toStore);
 }
