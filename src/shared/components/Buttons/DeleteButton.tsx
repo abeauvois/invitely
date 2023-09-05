dd63549bf991b8796d5ee233c8688553efc0b5e7
boostup <boostup@gmail.com>
2023-09-05 11:45:33 +0200

    UI: FormCard action buttons are now more discreet

--- src/shared/components/Buttons/DeleteButton.tsx
+++ src/shared/components/Buttons/DeleteButton.tsx
@@ -4,6 +4,7 @@ import { TrashIcon } from "@radix-ui/react-icons";
 export const DeleteButton = ({ onDelete }) => (
     <Button
         variant="destructive"
+        size="icon"
         onClick={onDelete}>
         <TrashIcon className="w-6 h-6" />
     </Button>
